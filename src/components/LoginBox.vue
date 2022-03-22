26ae98fc8c696abdcec5669c176721466b2d5e60
Diego Saldano <dsaldano@preminens.com.ar>
2022-03-22 20:19:56 -0300

    feat: create layout to login view, components and img logo

---
+++ src/components/LoginBox.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md3>
+      <v-card class="elevation-12">
+        <img
+            class="logo"
+            src="../assets/cintelink-logo-big.png"
+        />
+          <v-card-text>
+            <v-form>
+                <v-text-field
+                  solo
+                  background-color="rgb(238 238 238)"
+                  name="login"
+                  label="Usuario"
+                  type="text"
+                  clearable
+                ></v-text-field>
+                <v-text-field
+                  solo
+                  background-color="rgb(238 238 238)"
+                  id="password"
+                  name="password"
+                  label="Contraseña"
+                  type="password"
+                  clearable
+                ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions class="containerButton">
+            <v-btn color="primary" to="/">Login</v-btn>
+          </v-card-actions>
+          <div>
+            <h5>¿Olvidaste tu contraseña?</h5>
+            <h5>Crear cuenta</h5>
+          </div>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'LoginBox',
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style>
+.logo {
+  margin: auto;
+  padding-top: 2em;
+  max-width: 180px !important;
+}
+
+.containerButton {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0px 8px !important;
+}
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>
-    Login
-  </div>
+  <v-container class="cintelinkBg content" grid-list-md text-center fluid>
+      <LoginBox/>
+  </v-container>
+
 </template>
 
 <script>
+import LoginBox from '../components/LoginBox.vue';
+export default {
+  components: {
+    LoginBox
+  },
+  name: 'Login',
+  data: () => ({
+    //
+  }),
+};
 </script>
+<style>
+.cintelinkBg {
+  background-image: url('../assets/cintelink-bg.jpg');
+  height: 100vh;
+  background-size: cover;
+}
+
+.content {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
